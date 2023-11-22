3d867e4395ab3a040f6e23289bf7840c435eff43
NickPGC <149609208+NickPGC@users.noreply.github.com>
2023-11-22 14:36:52 -0400

    components-1

---
+++ components-1/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+    import ButtonCounter from './assets/components/ButtonCounter.vue';
+    import BlogPost from './assets/components/BlogPost.vue';
+</script>
+
+<template> 
+    <div class="container">
+      <h1> APP </h1>
+      <ButtonCounter> </ButtonCounter>
+      <button-counter></button-counter>
+
+      <BlogPost title="Post 1" id="1" body="descripcion 1" colorText="primary"/>
+      <BlogPost title="Post 2" id="2" body="descripcion 2" colorText="secondary"/>
+      <BlogPost title="Post 3" id="3" body="descripcion 3" colorText="success"/>
+      <BlogPost title="Post 4" id="4" body="descripcion 4" colorText="primary"/>
+    </div>
+</template>
---
+++ components-1/src/assets/components/BlogPost.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+defineProps(['title', 'id', 'body', 'colorText'])
+
+</script>
+
+<template> 
+    <div class="card">
+        <div class="card-body">
+            <h5 class="card-title" :class="`text-${colorText}`"> {{ id }} - {{ title }} </h5>
+            <p> {{ body }} </p>
+        </div>
+    </div>
+</template>
